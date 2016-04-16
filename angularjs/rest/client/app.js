67f81bbe80d258e1ba793c9ad695e7d5c655e7a7
Alex M <alex.margaritescu@tellence.net>
2016-04-16 16:03:10 +0300

    enhance AngularJS basic REST tutorial

--- angularjs/rest/client/app.js
+++ angularjs/rest/client/app.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', ['ngResource']);
 
-myApp.controller('MyController', function ($scope, userService) {
-    $scope.users = userService.query();    
+myApp.controller('MyController', function ($scope, userService) {  
+    $scope.loading = true;
+    userService.query().$promise.then(function(result) {
+        $scope.loading = false;
+        $scope.users = result;    
+    });
     $scope.demo = 'This is a demo';
 });
 
